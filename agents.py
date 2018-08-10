38d9cb67e70295cd9e37ff7d48e6ba13de09c216
Eloy Ruiz <eloy-javier.ruiz-donayre@hsrw.org>
2018-08-10 10:09:43 +0200

    Add project files.

---
+++ agents.py
@@ -0,0 +1,201 @@
+import random
+import numpy as np
+
+from mesa import Agent
+
+class Animal(Agent):
+    '''
+    An animal that goes around looking for Food Patches and Sleep Patches,
+    '''
+    def __init__(self, unique_id, model, pos, genomearr):
+        super().__init__(unique_id, model)
+        self.pos            = pos
+        self.direction      = random.randint(0,23)
+        self.sleep_energy   = 0
+        self.food_energy    = 0
+        self.hour_of_day    = 0
+        self.circadian_rythm = genomearr
+        self.objective      = 'feeding'
+        self.fitness_food   = .5
+        self.fitness_sleep  = .5
+        self.fitness        = .5
+        self.mode           = 0
+        self.lookingto      = None
+        self.looking()
+
+        self.sleep_factor   = 1
+        self.food_factor    = 1
+
+    def step(self):
+        # Find hour of the day
+        self.will_move      = True
+        self.mode           = 0
+        self.hour_of_day    = int ( ( self.model.schedule.time % 24 ) / 60 )
+        self.define_objective()
+        self.behaviour_about_objective()
+
+        if self.will_move:
+            self.sleep_energy   = self.sleep_energy - ( 1 * self.sleep_factor  )
+            self.food_energy    = self.food_energy  - ( 1 * self.food_factor   )
+            self.decide_direction()
+            self.move()
+
+        self.calculate_fitness()
+        self.looking()
+        
+    def define_objective(self):
+        if self.circadian_rythm[self.hour_of_day] == 'sleeping' :
+            self.objective      = 'sleeping'
+        if self.circadian_rythm[self.hour_of_day] == 'feeding' :
+            self.objective      = 'feeding'
+        if self.circadian_rythm[self.hour_of_day] == 'flexible' :
+            if self.fitness_sleep + .1 < self.fitness_food:
+                self.objective  = 'sleeping'
+            elif self.fitness_food + .1 < self.fitness_sleep:
+                self.objective  = 'feeding'
+            else:
+                pass
+
+    def behaviour_about_objective(self):
+        this_cell   = self.model.grid.get_cell_list_contents([self.pos])
+        food        = [obj for obj in this_cell if isinstance(obj, FoodPatch)]
+        sleep       = [obj for obj in this_cell if isinstance(obj, SleepPatch)]
+
+        if ( ( self.objective   == 'sleeping' )   and (len(sleep) > 0) ):
+            self.sleep_energy   = self.sleep_energy + 3 * self.sleep_factor
+            self.mode           = -10
+            self.will_move      = False
+        elif ( ( self.objective   == 'feeding' )    and (len(food) > 0) ):
+            self.food_energy    = self.food_energy  + (.2 * food[0].death_ticks) * self.food_factor
+            food[0].death_ticks -= 1
+            self.will_move      = False
+            self.mode           = 10
+            if food[0].death_ticks < 0:
+                self.model.grid._remove_agent(self.pos, food[0])
+                self.model.new_foodpatch()
+            else:
+                pass
+        else:
+            pass
+
+    def decide_direction(self):
+        inrange = False
+        while not inrange:
+            a = random.randint(-1,1)
+            self.direction  = self.direction + a
+            self.direction  = self.direction % 24
+            dx = 0
+            dy = 0
+            if ( self.direction in [23, 0, 1]):
+                dx = 1
+                dy = 0
+            if ( self.direction in [2, 3, 4]):
+                dx = 1
+                dy = 1
+            if ( self.direction in [5, 6, 7]):
+                dx = 0
+                dy = 1
+            if ( self.direction in [8, 9, 10]):
+                dx = -1
+                dy = 1
+            if ( self.direction in [11, 12, 13]):
+                dx = -1
+                dy = 0
+            if ( self.direction in [14, 15, 16]):
+                dx = -1
+                dy = -1
+            if ( self.direction in [17, 18, 19]):
+                dx = 0
+                dy = -1
+            if ( self.direction in [20, 21, 22]):
+                dx = 1
+                dy = -1
+            (x, y) = self.pos
+            nx = x + dx
+            ny = y + dy
+            if not self.model.grid.out_of_bounds((nx,ny)):
+                inrange = True
+    
+    def move(self):
+        dx = 0
+        dy = 0
+        if ( self.direction in [23, 0, 1]):
+            dx = 1
+            dy = 0
+        if ( self.direction in [2, 3, 4]):
+            dx = 1
+            dy = 1
+        if ( self.direction in [5, 6, 7]):
+            dx = 0
+            dy = 1
+        if ( self.direction in [8, 9, 10]):
+            dx = -1
+            dy = 1
+        if ( self.direction in [11, 12, 13]):
+            dx = -1
+            dy = 0
+        if ( self.direction in [14, 15, 16]):
+            dx = -1
+            dy = -1
+        if ( self.direction in [17, 18, 19]):
+            dx = 0
+            dy = -1
+        if ( self.direction in [20, 21, 22]):
+            dx = 1
+            dy = -1
+        (x, y) = self.pos
+        new_position = (x + dx, y + dy)
+        self.model.grid.move_agent(self, new_position)
+
+    def calculate_fitness(self):
+        self.fitness_food   = 1 / ( 1 + np.exp(self.food_energy * (-1/100) ) )
+        self.fitness_sleep  = 1 / ( 1 + np.exp(self.sleep_energy * (-1/100) ) )    
+        self.fitness        = (self.fitness_food + self.fitness_sleep) / 2
+
+    def looking(self):
+        if ( self.direction in [23, 0, 1]):
+            self.lookingto = 'RIGHT'
+        if ( self.direction in [2, 3, 4]):
+            self.lookingto = 'TOP-RIGHT'
+        if ( self.direction in [5, 6, 7]):
+            self.lookingto = 'TOP'
+        if ( self.direction in [8, 9, 10]):
+            self.lookingto = 'TOP-LEFT'
+        if ( self.direction in [11, 12, 13]):
+            self.lookingto = 'LEFT'
+        if ( self.direction in [14, 15, 16]):
+            self.lookingto = 'BOTTOM-LEFT'
+        if ( self.direction in [17, 18, 19]):
+            self.lookingto = 'BOTTOM'
+        if ( self.direction in [20, 21, 22]):
+            self.lookingto = 'BOTTOM-RIGHT'
+
+class FoodPatch(Agent):
+    '''
+    A patch with food
+    '''
+
+    def __init__(self, unique_id, model, pos, death_ticks):
+        '''
+        Creates a new Food Patch
+        '''
+        super().__init__(unique_id, model)
+        self.pos = pos
+        self.death_ticks = death_ticks
+
+    def step(self):
+        pass
+
+class SleepPatch(Agent):
+    '''
+    A patch to sleep
+    '''
+    def __init__(self, unique_id, model, pos):
+        '''
+        Creates a new Sleep Patch
+        '''
+        super().__init__(unique_id, model)
+        self.pos = pos
+
+    def step(self):
+        pass
---
+++ model.py
@@ -0,0 +1,206 @@
+import random
+import numpy as np
+
+from mesa import Model
+from mesa.space import MultiGrid
+from mesa.datacollection import DataCollector
+from mesa.time import RandomActivation
+
+from agents import FoodPatch, SleepPatch, Animal
+
+class SleepAnimals(Model):
+    '''
+    Analysis of the evolution of sleep in animals
+    '''
+
+    height = 40
+    width = 40
+
+    number_food_patch = 40
+    number_sleep_patch = 40
+
+    interdistance_factor = 0.4
+    intradistance_factor = 0.2
+
+    ticks_to_death = 30
+
+    
+    
+    def __init__(self, model_id , genome, width = 40, height = 40, 
+                 number_food_patch = 40, number_sleep_patch = 40,
+                 interdistance_factor = 0.35, intradistance_factor = 0.2):
+        super().__init__()
+        
+        # Setting Parameters
+        self.model_id = model_id
+        self.width = width
+        self.height = height
+        self.number_food_patch = number_food_patch
+        self.number_sleep_patch = number_sleep_patch
+        self.interdistance_factor = interdistance_factor
+        self.intradistance_factor = intradistance_factor
+
+        self.genome = genome
+
+        self.fp_center_x = 0
+        self.fp_center_y = 0
+        self.sp_center_x = 0
+        self.sp_center_y = 0
+
+        self.current_id_food_patch = 0
+        self.current_id_sleep_patch = 0
+
+        self.schedule = RandomActivation(self)
+        self.grid = MultiGrid(self.width, self.height, torus = False)
+        self.datacollector = DataCollector(
+           agent_reporters={"Food_fitness"  : lambda a: a.fitness_food,
+                            "Sleep_fitness" : lambda a: a.fitness_sleep,
+                            "Fitness"       : lambda a: a.fitness,
+                            "Mode"          : lambda a: a.mode,
+                            "Direction"     : lambda a: a.lookingto})
+        
+        # Picking Centers for Food and Sleep Patches
+        self.interdistance = 0
+        self.intradistance = 0
+        self.findingcenters()
+
+        # Populating Food Patches
+        self.available_food_patches = self.grid.get_neighborhood( (self.fp_center_x, self.fp_center_y), False, True, self.intradistance )
+        i = 0
+        while i < self.number_food_patch:
+            self.new_foodpatch()
+            i += 1
+
+        # Populating Sleep Patches
+        self.available_sleep_patches = self.grid.get_neighborhood( (self.sp_center_x, self.sp_center_y), False, True, self.intradistance )
+        i = 0
+        while i < self.number_sleep_patch:
+            current_spot = random.choice(self.available_sleep_patches)
+            if not self.grid.is_cell_empty(current_spot):
+                continue
+            sleep_patch = SleepPatch( self.next_id_sleeppatch(), self, current_spot )
+            self.grid.place_agent( sleep_patch, current_spot )
+            i += 1
+        
+        # Adding Animal to the world
+        current_spot = random.choice( self.grid.empties )
+        animal = Animal( self.next_id(), self, current_spot, self.genome )
+        self.grid.place_agent( animal, current_spot )
+        self.schedule.add( animal )
+        
+
+    def step(self):
+        '''Advance the model by one step.'''
+        self.datacollector.collect(self)
+        self.schedule.step()
+
+    def findingcenters(self):
+        max_manhattan_length = self.height + self.width
+        self.interdistance = int(max_manhattan_length * self.interdistance_factor)
+        self.intradistance = int(max_manhattan_length * self.intradistance_factor)
+
+        fp_center_x = 0
+        fp_center_y = 0
+        sp_center_x = 0
+        sp_center_y = 0
+        
+        centers_selected = False
+
+        while not centers_selected:
+            fp_center_x = random.randrange(self.width)
+            fp_center_y = random.randrange(self.height)
+            fp_center = (fp_center_x, fp_center_y)
+            
+            available_spots_food = self.grid.get_neighborhood(fp_center, False, False, self.intradistance)
+            if len(available_spots_food) < 80:
+                continue
+            
+            list_centers_sp = list( set( self.grid.get_neighborhood(fp_center, False, False, self.interdistance + 1)) - set( self.grid.get_neighborhood(fp_center, False, False, self.interdistance - 1) ) )
+            if len(list_centers_sp) < 5:
+                continue
+
+            sp_center = random.choice(list_centers_sp)
+            (sp_center_x, sp_center_y) = sp_center
+
+            available_spots_sleep = self.grid.get_neighborhood(sp_center, False, False, self.intradistance)
+            if len(available_spots_sleep) < 80:
+                continue
+
+            centers_selected = True
+
+        self.fp_center_x = fp_center_x
+        self.fp_center_y = fp_center_y
+        self.sp_center_x = sp_center_x
+        self.sp_center_y = sp_center_y
+
+    def next_id_foodpatch(self):
+        """ Return the next unique ID for food patches, increment current_id"""
+        self.current_id_food_patch += 1
+        a = 'M' + str(self.model_id) + 'F' + str(self.current_id_food_patch)
+        return a
+
+    def next_id_sleeppatch(self):
+        """ Return the next unique ID for sleep patches, increment current_id"""
+        self.current_id_sleep_patch += 1
+        a = 'M' + str(self.model_id) + 'S' + str(self.current_id_sleep_patch)
+        return a
+
+    def new_foodpatch(self):
+        spot_found = False
+        while not spot_found :
+            current_spot = random.choice(self.available_food_patches)
+            if not self.grid.is_cell_empty(current_spot):
+                continue
+            food_patch = FoodPatch(self.next_id_foodpatch(), self, current_spot, SleepAnimals.ticks_to_death)
+            self.grid.place_agent(food_patch, current_spot)
+            spot_found = True
+
+    def arrayRGB_clusters(self):
+        available_spots = np.full( (self.grid.width , self.grid.height , 3) , 255)
+        for coordinates in self.available_sleep_patches:
+            (x, y) = coordinates
+            available_spots[x][y]= [100, 0, 150]
+        for coordinates in self.available_food_patches:
+            (x, y) = coordinates
+            available_spots[x][y]= [10, 150, 0]
+        return available_spots
+
+    def arrayRGB_display(self):
+        RGBdisplay = np.full((self.grid.width, self.grid.height,3), 255)
+        for cell in self.grid.coord_iter():
+            cell_content, x, y = cell
+            a = list(cell_content)
+            if len(a) == 0:
+                RGBdisplay[x][y] = [255,255,255]
+            elif len(a) == 1:
+                if isinstance(a[0], SleepPatch):
+                    RGBdisplay[x][y] = [100,0,150]
+                elif isinstance(a[0], FoodPatch):
+                    RGBdisplay[x][y] = [10,150,0]
+                elif isinstance(a[0], Animal):
+                    RGBdisplay[x][y] = [0,0,0]
+                else:
+                    pass
+            else:
+                pass
+        return RGBdisplay
+
+    def array2D_display(self):
+        display2D = np.zeros((self.grid.width, self.grid.height))
+        for cell in self.grid.coord_iter():
+            cell_content, x, y = cell
+            a = list(cell_content)
+            if len(a) == 0:
+                display2D[x][y] = 0
+            elif len(a) == 1:
+                if isinstance(a[0], SleepPatch):
+                    display2D[x][y] = 10
+                elif isinstance(a[0], FoodPatch):
+                    display2D[x][y] = 20
+                elif isinstance(a[0], Animal):
+                    display2D[x][y] = 30
+                else:
+                    pass
+            else:
+                pass
+        return display2D
